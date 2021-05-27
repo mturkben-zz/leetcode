25cdd79bed42312bd13c981e82d53b1e0a3ca574
mturkben <mturkben721@gmail.com>
2021-05-27 22:13:01 +0300

    First Commit

--- leetcode-javascript/AddTwoNumbers.js
+++ leetcode-javascript/AddTwoNumbers.js
@@ -18,23 +18,18 @@ function ListNode(val, next) {
 
 function addTwoNumbers(l1, l2) {
   const _ = l1.length >= l2.length ? l1 : l2;
-  let val = [];
   let addOne = false;
-
-
   let result = root = {};
 
   for (let i = 0; i <= _.length; i++) {
     let x = l1[i] ? l1[i] : 0;
     let y = l2[i] ? l2[i] : 0;
-
     let count = x + y;
 
     if (addOne) {
       count = count + 1;
       addOne = false;
     }
-
     if (count >= 10) {
       addOne = true;
       count = count - 10;
@@ -42,10 +37,11 @@ function addTwoNumbers(l1, l2) {
 
     result.next = new ListNode(count);
     result = result.next;
-    val.push(count);
   }
 
   return root.next;
 }
 
-console.log(addTwoNumbers([2,4,3],[5,6,4]));
+console.log(addTwoNumbers([2,4,3],[5,6,4])); // Output: [7,0,8]
+//console.log(addTwoNumbers([0],[0])); Output: [0]
+//console.log(addTwoNumbers([9,9,9,9,9,9,9],[9,9,9,9])); Output: [8,9,9,9,0,0,0,1]
